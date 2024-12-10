2f5885fc245b343df803e8e4e7af04b9d4b4b892
subhashini006 <subhashini2ammu@gmail.com>
2024-12-10 14:13:39 +0530

    Add files via upload

---
+++ lab 5.py
@@ -0,0 +1,8 @@
+#1
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+num=int(input('enter the  number:'))
+print(f"The factorial is {factorial(num)}")
